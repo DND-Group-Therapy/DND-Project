eb3327fbddfb1e2bb9ad3d6da1a8d33f226cbbc5
Laci <laciwright99@gmail.com>
2024-04-20 17:30:26 -0500

    test

---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="links" style={{backgroundColor: "beige"}}>
+                <a href="/">Home</a>
+                <a href="/create">New Sidekick</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
